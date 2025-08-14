7aab68e5668b018b5da3f17a4c135f6a5f535302
himani157 <natashahp3947@gmail.com>
2025-08-14 20:59:28 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ import { fileURLToPath } from "url";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;;
+const PORT = process.env.PORT || 5000;
 //const __dirname = path.resolve();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
